82db46c9d79e7049807eef3255191791e0179961
Athulsaji28 <athulsaji28@gmail.com>
2023-10-26 12:43:33 +0530

    initial commit

---
+++ src/main/java/dev/movieDetails/movies/Reviews.java
@@ -0,0 +1,22 @@
+package dev.movieDetails.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Reviews {
+	
+	@Id
+	private ObjectId id;
+	private String body;
+
+}
